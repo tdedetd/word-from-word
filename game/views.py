6239cfd13f3e5653eefeaac39d72eba6f2e6a4ac
tdedetd <ddimaa13@gmail.com>
2018-02-10 23:08:05 +0300

    - Базовая страница - Подключен jquery

--- game/views.py
+++ game/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
