139fb861e05d89e0a7082eea0be11496d123a860
tdedetd <ddimaa13@gmail.com>
2018-02-26 21:58:04 +0300

    - Добавлена возможность ввода слов посредством нажатия на буквы - Показ оставшихся букв - Подсвечивание кнопок при наведении - Стилизация кнопок для Safari - Доработка игрового интерфейса - Прочие доработки

--- game/static/js/game.js
+++ game/static/js/game.js
@@ -1,12 +1,101 @@
 "use strict";
 
-let wordsSolved;
+const CYRILLIC = {
+    1072: "а",
+    1073: "б",
+    1074: "в",
+    1075: "г",
+    1076: "д",
+    1077: "е",
+    1105: "е",
+    1078: "ж",
+    1079: "з",
+    1080: "и",
+    1081: "й",
+    1082: "к",
+    1083: "л",
+    1084: "м",
+    1085: "н",
+    1086: "о",
+    1087: "п",
+    1088: "р",
+    1089: "с",
+    1090: "т",
+    1091: "у",
+    1092: "ф",
+    1093: "х",
+    1094: "ц",
+    1095: "ч",
+    1096: "ш",
+    1097: "щ",
+    1098: "ъ",
+    1099: "ы",
+    1100: "ь",
+    1101: "э",
+    1102: "ю",
+    1103: "я",
+    1040: "А",
+    1041: "Б",
+    1042: "В",
+    1043: "Г",
+    1044: "Д",
+    1045: "Е",
+    1025: "Е",
+    1046: "Ж",
+    1047: "З",
+    1048: "И",
+    1049: "Й",
+    1050: "К",
+    1051: "Л",
+    1052: "М",
+    1053: "Н",
+    1054: "О",
+    1055: "П",
+    1056: "Р",
+    1057: "С",
+    1058: "Т",
+    1059: "У",
+    1060: "Ф",
+    1061: "Х",
+    1062: "Ц",
+    1063: "Ч",
+    1064: "Ш",
+    1065: "Щ",
+    1066: "Ъ",
+    1067: "Ы",
+    1068: "Ь",
+    1069: "Э",
+    1070: "Ю",
+    1071: "Я"
+};
+
+let wordsSolved, wordInput;
 
 $(document).ready(() => {
     wordsSolved = +$("#words-solved").text();
+    wordInput = $("#word-input");
 
     $("#submit-word-button").on("click", () => {
         submitWord();
+    });
+
+    $("#backspace-button").on("click", () => {
+        backspace(true);
+    });
+
+    $("#clear-button").on("click", () => {
+        wordInput.val("");
+        enableAllLetters();
+    });
+
+    wordInput.keypress(function(e) {
+        if (e.keyCode == 13) {
+            submitWord();
+        } else if (e.keyCode == 8) {
+            backspace(false);
+        } else {
+            disableLetter(CYRILLIC[e.which]);
+        }
     });
 
     $("body").keypress(function(e) {
@@ -14,12 +103,36 @@ $(document).ready(() => {
             submitWord();
         }
     });
+
+    $(".letters__item").on("click", function() {
+        let letterBlock = $(this);
+
+        if (!letterBlock.hasClass("letters__item_disabled")) {
+            letterBlock.addClass("letters__item_disabled");
+            wordInput.val(wordInput.val() + letterBlock.text().toLowerCase());
+        }
+    });
 });
 
+function backspace(removeLastLetter) {
+    if (wordInput.val() == "") {
+        return;
+    }
+
+    let word = wordInput.val();
+    enableLetter(word[word.length - 1]);
+
+    if (removeLastLetter) {
+        word = word.substr(0, word.length - 1);
+    }
+
+    wordInput.val(word);
+}
+
 function submitWord() {
-    let wordInput = $("#word-input");
     let word = wordInput.val().toLowerCase();
     wordInput.val("");
+    enableAllLetters();
 
     if (word.trim() == "") {
         return;
@@ -67,4 +180,58 @@ function insertSolvedWord(word) {
             div.insertBefore($(words[wordCount]));
         }
     }
+}
+
+function enableAllLetters() {
+    $(".letters__item").removeClass("letters__item_disabled");
+}
+
+/**
+ * Активирует первый втретившийся неактивный блок с указанной буквой.
+ * В последствии этого он станет кликабельным.
+ * Возаращает признак успешности.
+ * @param {string} letter буква
+ */
+function enableLetter(letter) {
+    return toogleLetter(true, letter);
+}
+
+/**
+ * Деактивирует первый втретившийся активный блок с указанной буквой.
+ * В последствии этого он станет некликабельным.
+ * Возаращает признак успешности.
+ * @param {string} letter буква
+ */
+function disableLetter(letter) {
+    return toogleLetter(false, letter);
+}
+
+/**
+ * Меняет состояние блока с первой встретившейся указанной буквой на указанное.
+ * Возаращает признак успешности.
+ * @param {boolean} enable true - активировать, false - деактивировать
+ * @param {string} letter буква
+ */
+function toogleLetter(enable, letter) {
+    letter = letter.toLowerCase();
+    let letters = $(".letters__item");
+    let letterBlock, letterDisabled, letterSuits;
+
+    for (let i = 0; i < letters.length; i++) {
+        letterBlock = $(letters[i]);
+        
+        letterDisabled = letterBlock.hasClass("letters__item_disabled");
+        letterSuits = letterBlock.text().toLowerCase() == letter;
+
+        if (enable == letterDisabled && letterSuits) {
+            
+            if (enable) {
+                letterBlock.removeClass("letters__item_disabled");
+            } else {
+                letterBlock.addClass("letters__item_disabled");
+            }
+            return true;
+        }
+    }
+    return false;
 }
--- game/static/js/lvl_select.js
+++ game/static/js/lvl_select.js
@@ -4,7 +4,7 @@ const LVL_CLASS = "col col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4";
 
 let lvlHtml, levels;
 let lvlCount = 0,
-    lvlLimit = 20,
+    lvlLimit = 30,
     typeId = 1,
     dirId = 1;
 
