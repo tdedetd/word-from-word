8c9286071b519f2d85b7af1f863f161614a94c5b
dmitry <ddimaa13@gmail.com>
2018-07-26 21:47:26 +0300

    added scripts which update statuses of words

---
+++ sql/disable_word.sql
@@ -0,0 +1,40 @@
+﻿DROP FUNCTION public.disable_word(integer);
+
+create function disable_word(in_word_id int) returns int as $$
+DECLARE
+	level_word_ids integer[];
+	level_ids integer[];
+BEGIN
+	-- получение списка подходящих слов для уровней, содержащих данное слово
+	-- 642 аи
+	level_word_ids = array(
+		SELECT lw.id
+		FROM level_word lw
+		WHERE lw.word_id = in_word_id
+	);
+
+	-- получение списка уровней
+	level_ids = array(
+		SELECT lw.level_id
+		FROM level_word lw
+		WHERE lw.word_id = in_word_id
+	);
+	
+	-- удаление слова из таблицы решений
+	DELETE FROM user_solution
+	WHERE array[user_solution.level_word_id] <@ level_word_ids;
+	
+	-- удаление слова из таблицы подходящих слов
+	DELETE FROM level_word
+	WHERE array[level_word.id] <@ level_word_ids;
+	
+	-- декремент общего количества слов из уровней слова
+	UPDATE levels
+	SET word_count = word_count - 1
+	WHERE array[id] <@ level_ids;
+
+	RETURN 1;
+
+END
+$$
+language plpgsql
---
+++ sql/enable_word.sql
@@ -0,0 +1,39 @@
+﻿-- DROP FUNCTION public.enable_word(integer);
+
+create function enable_word(in_word_id int) returns int as $$
+DECLARE
+	gotten_word text;
+	level_ids integer[];
+BEGIN
+	IF NOT EXISTS (SELECT * FROM words w WHERE w.id = in_word_id) THEN
+		RAISE EXCEPTION 'Указан неверный id слова (%)', in_word_id;
+	END IF;
+
+	-- получение слова по id
+	SELECT w.word
+	INTO gotten_word
+	FROM words w
+	WHERE w.id = in_word_id;
+	
+	-- получение списка уровней, для которых подходит указанное слово
+	level_ids := array(
+		SELECT l.id
+		FROM levels l, words w
+		WHERE includes(w.word, 'аи') and l.word_id = w.id
+	);
+	
+	-- добавление слова в подходящие слова для полученных уровней
+	INSERT INTO level_word (level_id, word_id)
+	SELECT l, in_word_id
+	FROM unnest(level_ids) as l;
+
+	-- инкрементирование общего кол-ва слов для полученных уровней
+	UPDATE levels
+	SET word_count = word_count + 1
+	WHERE array[id] <@ level_ids;
+
+	RETURN 1;
+	
+END
+$$
+language plpgsql
