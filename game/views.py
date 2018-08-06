e720ba58974fc688e70b58d975c73b78461a1ce8
dmitry <ddimaa13@gmail.com>
2018-08-06 23:39:31 +0300

    uncommented registration

--- game/views.py
+++ game/views.py
@@ -83,8 +83,7 @@ def register(request):
 
 def redirect_to_register(request):
     from django.shortcuts import redirect, reverse
-    # return redirect(reverse('register'))
-    return redirect(reverse('home'))
+    return redirect(reverse('register'))
 
 def checklogin(request, login):
     """
