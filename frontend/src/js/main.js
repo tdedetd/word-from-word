8401cb3878efd90af7f3330a439366391f8e29da
tdedetd <tdedetd@gmail.com>
2021-05-22 20:48:24 +0300

    hotfix: init cookie modal

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -4,8 +4,8 @@ import { displayXpInfo } from './shared/xp';
 document.addEventListener('DOMContentLoaded', () => {
     displayXpInfo();
 
+    const cookieModal = new Modal('cookie-modal');
     if (!localStorage.getItem('cookieModalShown')) {
-        const cookieModal = new Modal('cookie-modal');
         setTimeout(() => {
             cookieModal.show();
             localStorage.setItem('cookieModalShown', true);
