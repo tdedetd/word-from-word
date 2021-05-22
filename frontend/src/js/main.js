55700a1a98773f53b28ced2bfd6a118122a610ae
tdedetd <tdedetd@gmail.com>
2021-05-22 20:02:14 +0300

    cookie modal

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,5 +1,14 @@
+import { Modal } from './shared/modal';
 import { displayXpInfo } from './shared/xp';
 
 document.addEventListener('DOMContentLoaded', () => {
     displayXpInfo();
+
+    if (!localStorage.getItem('cookieModalShown')) {
+        const cookieModal = new Modal('cookie-modal');
+        setTimeout(() => {
+            cookieModal.show();
+            localStorage.setItem('cookieModalShown', true);
+        }, 3000);
+    }
 });
